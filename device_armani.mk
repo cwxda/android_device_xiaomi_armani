f33ab08fe18a01f112e257ea617f4ccbb9f0cb56
darknightmare <rubentkd98@gmail.com>
2014-07-29 14:57:22 +0200

    mkvendor.sh: Initial commit of armani

--- device_armani.mk
+++ device_armani.mk
@@ -1,27 +1,24 @@
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, device/xiaomi/msm8226-common/msm8226.mk)
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/xiaomi/armani/overlay
 
 LOCAL_PATH := device/xiaomi/armani
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
-# Kernel
 PRODUCT_COPY_FILES += \
-    device/xiaomi/armani/kernel:kernel \
-    device/xiaomi/armani/dt.img:dt.img
-
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+    $(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_armani
 PRODUCT_DEVICE := armani
-PRODUCT_LOCALES := es_ES
-PRODUCT_LOCALES += xhdpi
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-$(call inherit-product, device/xiaomi/msm8226-common/keylayout/keylayout.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/armani/armani-vendor.mk)
